5596006a52b2ab70ae70fff8de0925c29ce26987
st1les <ka882@drexel.edu>
2021-01-26 13:37:39 -0500

    Quote generator
    Great project!

---
+++ quotes.js
@@ -0,0 +1,15 @@
+const localQuotes = [
+    {
+        text: 'A memory without the emotional charge is called wisdom.',
+        author: 'Joe Dispenza',
+    },
+    {
+        text: 'Your thoughts are incredibly powerful. Choose yours wisely.',
+        author: 'Joe Dispenza',
+    },
+    {
+        text: 'I’m always opening 1000 doors. Everyone sees the 10 that stay open. That’s the key to growth.',
+        author: 'Jay Shetty',
+    },
+    
+]
